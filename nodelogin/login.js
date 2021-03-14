82dba9938f34eed85dff39eaf74c8d16bdd2914e
WingsL666 <72357231+WingsL666@users.noreply.github.com>
2021-03-14 00:50:14 -0800

    Debug the signIn function

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -26,13 +26,21 @@ class Login {
     });
   }
 
+ /*
   signIn(log_user_email, log_user_id, log_hashed_password, log_salt) {
     return this.all(
       "Insert Into login (log_user_email, log_user_id, log_hashed_password, log_salt)" +
         "VALUES(?, ?, ?, ?)",
       [log_user_email, log_user_id, log_hashed_password, log_salt]
     );
+  }*/
+  signIn(log_user_email, log_user_id, log_hashed_password, log_salt) {
+    return this.all(
+      "Insert Into login (log_user_email, log_user_id, log_hashed_password, log_salt) VALUES (?, ?, ?, ?)",
+      [log_user_email, log_user_id, log_hashed_password, log_salt]
+    );
   }
+  
 }
 
 module.exports = Login;
