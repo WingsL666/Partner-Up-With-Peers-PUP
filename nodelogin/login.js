1aad15fb7a0a838b9e1ff55eaa67f0a4d302d77a
raguilar27 <rogelioaguilar208@gmail.com>
2021-03-13 16:59:01 -0800

    added login hash

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -1,5 +1,50 @@
-var mysql = require('mysql');
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var path = require('path');
+const express = require("express");
+const db = require("./database.js");
+const bcrypt = require("bcrypt");
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+
+//enables parsing of json objects
+app.use(express.json());
+
+const users = [];
+
+//goes to sign in page
+app.get("/api/login", (req, res) => {
+  res.send("Sign in page");
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(password);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/api/login/auth", async (req, res) => {
+  const user = users.find((user) => (user.name = req.body.name));
+  if (user == null) {
+    return res.status(400).send("Cannot Find User");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Successs!");
+    } else {
+      res.send("Incorrect Password");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
+
+module.exports = login;
