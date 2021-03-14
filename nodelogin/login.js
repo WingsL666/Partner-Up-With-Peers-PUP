45668c3b72755b2bf7016b770c3ab59b5866fadb
raguilar27 <rogelioaguilar208@gmail.com>
2021-03-13 19:59:59 -0800

    changed login.js to store queries

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -1,50 +1,30 @@
-const express = require("express");
-const db = require("./database.js");
-const bcrypt = require("bcrypt");
 const sqlite3 = require("sqlite3").verbose();
+const Promise = require("bluebird");
 
-const app = express();
-
-//enables parsing of json objects
-app.use(express.json());
-
-const users = [];
-
-//goes to sign in page
-app.get("/api/login", (req, res) => {
-  res.send("Sign in page");
-});
-
-app.post("/api/login", async (req, res) => {
-  try {
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    console.log(password);
-    console.log(salt);
-    console.log(hashedPassword);
-    const user = { name: req.body.name, password: hashedPassword };
-    users.push(user);
-    res.status(201).send();
-  } catch {
-    res.status(500).send();
+class Login {
+  constructor(dbFilePath) {
+    this.db = new sqlite3.Database(dbFilePath, (err) => {
+      if (err) {
+        console.log("Could not connect to database", err);
+      } else {
+        console.log("Connected to database");
+      }
+    });
   }
-});
 
-app.post("/api/login/auth", async (req, res) => {
-  const user = users.find((user) => (user.name = req.body.name));
-  if (user == null) {
-    return res.status(400).send("Cannot Find User");
+  all(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, rows) => {
+        if (err) {
+          console.log("Error running sql: " + sql);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
   }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.send("Successs!");
-    } else {
-      res.send("Incorrect Password");
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
+}
 
 module.exports = login;
